497e7605ad642b668397ecd1001bcd1c43975ada
Jordan Dukart <jordan@discoverygarden.ca>
2013-09-19 21:30:02 +0000

    Coding standards, theme implementation.

---
+++ theme/islandora-solr-metadata-display.tpl.php
@@ -0,0 +1,17 @@
+<fieldset class="collapsible collapsed">
+  <legend><span class="fieldset-legend"><?php print t('Details'); ?></span></legend>
+  <div class="fieldset-wrapper">
+    <dl xmlns:dcterms="http://purl.org/dc/terms/" class="islandora-inline-metadata islandora-metadata-fields">
+      <?php $row_field = 0; ?>
+      <?php foreach($solr_fields as $value): ?>
+        <dt class="<?php print $row_field == 0 ? ' first' : ''; ?>">
+          <?php print $value['display_label']; ?>
+        </dt>
+        <dd class="<?php print $row_field == 0 ? ' first' : ''; ?>">
+          <?php print implode('<br/>', $value['value']); ?>
+        </dd>
+        <?php $row_field++; ?>
+      <?php endforeach; ?>
+    </dl>
+  </div>
+</fieldset>
