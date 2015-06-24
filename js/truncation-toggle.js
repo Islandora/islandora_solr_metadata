716b5a9179437b4c88559c7a3bf64707e116dc1f
Adam Vessey <adam@discoverygarden.ca>
2015-06-24 18:11:07 -0300

    Add additional input filter for use.

--- js/truncation-toggle.js
+++ js/truncation-toggle.js
@@ -5,16 +5,13 @@
     attach: function (context, settings) {
       $('.toggle-wrapper', context).once('truncation-toggle', function() {
         var $this = $(this);
-        $this.css({
-          'list-style-type': 'none',
-          'padding': 0,
-          'margin': 0
-        }).find('> li').hide().first().show();
+        $this.find('> span').hide().first().show();
       });
       $('.toggle-wrapper .toggler', context).once('truncation-toggle', function(){
         var $this = $(this);
-        $this.click(function (){
-          $this.closest('.toggle-wrapper').find('> li').toggle();
+        $this.click(function (event){
+          event.preventDefault();
+          $this.closest('.toggle-wrapper').find('> span').toggle();
         });
       });
     }
